4688b517b8fb1a6f52c10a6d974dbf0dd840a4c4
drklokesh28 <drklokesh28@gmail.com>
2026-08-24 12:11:20 +0530

    Update Courses.py

--- Courses.py
+++ Courses.py
@@ -1,25 +1,23 @@
 import streamlit as st
-import pandas as pd
 
 def main_layout():
-    st.subheader("You Currently Decided To Learn".upper(),divider="orange",text_alignment="center")
-    col1,col2=st.columns([1,2],border=True,gap="small")
-    options=col1.radio("Select The Unit To Learn",["Unit 1","Unit 2","Unit 3","Unit 4","Unit 5"],horizontal=True)
-    select_topic=col2.selectbox("Select the topic that you wanted to learn",fetchTopics(options))
-    fetch_vedio=col2.video(fetchVideo(options,select_topic))
-    col2.text(fetchDescription(options,select_topic))
+    st.subheader("YOU CURRENTLY DECIDED TO LEARN", divider="orange", text_alignment="center")
+    col1, col2 = st.columns([1, 2], border=True, gap="small")
+    
+    options = col1.radio("Select The Unit To Learn", ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5"], horizontal=True)
+    
+    unit_key = options.lower()
+    unit_data = st.session_state.get(unit_key, [])
+    
+    if not unit_data:
+        col2.info("No content available for this unit.")
+        return
 
-def fetchTopics(unit):
-    unit=unit.lower()
-    df=pd.DataFrame(st.session_state[unit])
-    return [x for x in df["topic_name"].values]
+    topic_names = [t["topic_name"] for t in unit_data if "topic_name" in t]
+    select_topic = col2.selectbox("Select the topic that you wanted to learn", topic_names)
 
-def fetchVideo(unit,topic_name):
-    unit=unit.lower()
-    df=pd.DataFrame(st.session_state[unit])
-    return df[df["topic_name"]==topic_name]["yt_link"].values[0]
-
-def fetchDescription(unit,topic_name):
-    unit=unit.lower()
-    df=pd.DataFrame(st.session_state[unit])
-    return df[df["topic_name"]==topic_name]["description"].values[0]
+    selected_data = next((t for t in unit_data if t.get("topic_name") == select_topic), None)
+    
+    if selected_data:
+        col2.video(selected_data.get("yt_link", ""))
+        col2.text(selected_data.get("description", ""))
