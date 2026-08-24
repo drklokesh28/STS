5906f574402d3aebc69aeda159348b7327ea69a7
drklokesh28 <drklokesh28@gmail.com>
2026-08-24 12:35:05 +0530

    Update Materials.py

--- Materials.py
+++ Materials.py
@@ -76,7 +76,7 @@ def main_layout():
                         # Verify local file exists
                         if os.path.exists(full_path):
                             # High-speed native PDF rendering
-                            st.pdf(full_path,height="stretch")
+                            st.pdf(full_path,height="stretch",width="stretch")
                             
                             # Fast download option via memory stream
                             with open(full_path, "rb") as f:
