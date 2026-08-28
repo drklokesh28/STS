995029ae4ee4c9ca5bccf9961a7af395c452f889
drklokesh28 <drklokesh28@gmail.com>
2026-08-28 11:28:04 +0530

    Update Courses.py

--- Courses.py
+++ Courses.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from advertisements import *
 
 def main_layout():
     st.subheader("YOU CURRENTLY DECIDED TO LEARN", divider="orange", text_alignment="center")
     col1, col2 = st.columns([1, 2], border=True, gap="small")
     
     options = col1.radio("Select The Unit To Learn", ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5"], horizontal=True)
+    with col1:
+        show_advertisement()
     
     unit_key = options.lower()
     unit_data = st.session_state.get(unit_key, [])
@@ -21,3 +24,5 @@ def main_layout():
     if selected_data:
         col2.video(selected_data.get("yt_link", ""))
         col2.text(selected_data.get("description", ""))
+        with col2:
+            show_advertisement()
