c52900efdb24aa4eef276373b651eddba4523ff2
drklokesh28 <drklokesh28@gmail.com>
2026-08-29 09:38:25 +0530

    Update advertisements.py

--- advertisements.py
+++ advertisements.py
@@ -63,3 +63,108 @@ def show_advertisement():
         height=270,
         scrolling=False
     )
+
+def show_advertisement1():
+    ad_html_1 = """
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+
+        <style>
+
+            html,
+            body {
+                margin: 0;
+                padding: 0;
+                background-color: transparent;
+                overflow: hidden;
+            }
+
+            .ad-container {
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+
+        <div class="ad-container-1">
+
+            <script>
+              atOptions = {
+                'key' : 'cfb3c02f1261778a6ed5018c09373968',
+                'format' : 'iframe',
+                'height' : 600,
+                'width' : 160,
+                'params' : {}
+              };
+            </script>
+            <script src="https://www.highrevenueformat.com/cfb3c02f1261778a6ed5018c09373968/invoke.js"></script>
+
+
+        </div>
+
+    </body>
+
+    </html>
+    """
+
+    components.html(
+        ad_html_1,
+        height=270,
+        scrolling=True
+    )
+
+def show_advertisement2():
+    ad_html_2 = """
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+
+        <style>
+
+            html,
+            body {
+                margin: 0;
+                padding: 0;
+                background-color: transparent;
+                overflow: hidden;
+            }
+
+            .ad-container {
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+
+        <div class="ad-container-1">
+
+            <script src="https://pl31019682.profitableratecpmnetwork.com/a5/ce/c1/a5cec18624bd720b75fc810fe28973e8.js"></script>
+
+
+        </div>
+
+    </body>
+
+    </html>
+    """
+
+    components.html(
+        ad_html_2,
+        height=270,
+        scrolling=True
+    )
